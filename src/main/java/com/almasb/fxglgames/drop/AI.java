f5f145e4dc625148cb0aaf9026b854d01fdf73fe
Nathan <nathan.balaz@stud.fh-campuswien.ac.at>
2021-01-03 19:47:22 +0100

    easyUpdate4

--- src/main/java/com/almasb/fxglgames/drop/AI.java
+++ src/main/java/com/almasb/fxglgames/drop/AI.java
@@ -2,36 +2,10 @@ package com.almasb.fxglgames.drop;
 
 import java.util.Random;
 
+//TODO --GewinnBedingung damit AI nicht endlos weiterschießt (Sieges Screen)? --Speichern der Schüsse der AI notwendig für extern oder speichert hitMethod Schüsse ab?
+
 
 public class AI {
-/*
-//Zugtimer??
-// https://github.com/GrahamBlanshard/AI-Battleship/blob/master/prograham/battleship/player/AIPlayer.java
-    public void placeShip()
-    {
-        Random generator = new Random();	//Used to simulate computer "guessing"
-        Boolean successful = false;
-        boolean direction;
-
-        // Iterate through ships and place them
-        for (Ship c : ships) {
-            do {
-                Integer orientation = generator.nextInt(2);
-                if(orientation == 0)
-                    direction = true; // Horizontal
-                else
-                    direction = false; // Vertical
-
-                Coordinate shipSpot = new Coordinate(generator.nextInt(grid.getGridSize()),generator.nextInt(grid.getGridSize()));
-
-                successful = c.placeShip(direction, grid, shipSpot.x(), shipSpot.y(), c.getSize());
-                grid.addShip(c.shipCoordsToGrid());
-            } while(!successful);
-        }
-    }
-
-
-*/
 
 
     public void EasyAI ()
@@ -39,25 +13,28 @@ public class AI {
 
             Random randomGenerator=new Random();
 
-            /*
-                int SchussX = randomGenerator.nextInt(10) + 1);
-                int SchussY = randomGenerator.nextInt(10) + 1); // +1 für Inklusion der Nummer 10
 
-                hit(SchussX, SchussY); //übergeben an HitMethode
+                int SchussX = randomGenerator.nextInt(10) + 1; // Ausgabe der Werte im Bereich 1-10
+                int SchussY = randomGenerator.nextInt(10) + 1;
 
-                if (tile.getProperties().getValue(SchussX) == true && tile.getProperties().getValue(SchussY) == true ) {
+/*
+                while (hit(SchussY, SchussX) == true) //übergeben an HitMethode und solange Treffer, Übergabe weiterer Schüsse an hitMethod
+                {
+                 int SchussX = randomGenerator.nextInt(10) + 1);
+                 int SchussY = randomGenerator.nextInt(10) + 1);
+                 }
+
+                 player.shoot; /Benachrichtigung über Zug des Spielers
+
+
+                //if (Board[SchussY][SchussX])
+                //if (tile.getProperties().getValue("x") == true && tile.getProperties().getValue(SchussY) == true ) {
 
                  }
 
 
-                if (HitMethode == 1) sofern Treffer auf Schiff führen
 
-                getroffen ?
-
-                übergeben an HitMethode
-
-
-             */
+*/
 
 
     }
@@ -74,13 +51,10 @@ public class AI {
 //Zusatzklausel, so lange schießen bis Leben aus ist?
 //Counterbalance zur x-Achsen abfragen, Computer weiß Schiffsleben
 //Schiffe als Objekte mit Leben true/false??
+
+
     public void MediumAI (AnzahlSpielfelder)
     {
-
-// nextInt ist normalerweise exklusive des höchsten wertes, deswegen plus +1
-
-        int RNG = ThreadLocalRandom.current().nextInt(min, max + 1);
-
 
 
 
@@ -89,13 +63,25 @@ public class AI {
 
 
     //Computer weiß wo Schiffe stehen und schießt nach gezieltem //versenken von Schiffen random im wasser umher
+
     public void HardAI (AnzahlSpielfelder)
     {
 
-        // nextInt ist normalerweise exklusive des höchsten wertes, deswegen plus +1
+        int SchussX = get.ShipX[1];
+        int SchussY = get.ShipY[1];
 
-        int RNG = ThreadLocalRandom.current().nextInt(min, max + 1);
+        while (hit(SchussY, SchussX) == true)
+              {
+                int SchussX = get.ShipX[i];
+                int SchussY = get.ShipY[i];
 
+                if (getHealthShip(i) == 0) {
+                break;
+                }
+              }
+
+
+          player.shoot; /Benachrichtigung über Zug des Spielers
 
 
 
@@ -105,6 +91,20 @@ public class AI {
 
  */
 }
+
+
+
+
+
+/*
+
+Zugtimer??
+
+andere Java Battleship Computer AI:
+
+https://github.com/GrahamBlanshard/AI-Battleship/blob/master/prograham/battleship/player/AIPlayer.java
+
+*/
 
 
 
